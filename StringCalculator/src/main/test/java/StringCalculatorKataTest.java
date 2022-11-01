6866eebdf4f8a7e3eb573431acada63dc804d8b2
KaiKrk <kaiqiang@outlook.fr>
2022-11-01 18:58:12 +0100

    add primary test ( Step 1 )

---
+++ StringCalculator/src/main/test/java/StringCalculatorKataTest.java
@@ -0,0 +1,32 @@
+import com.exalt.company.StringCalculatorKata;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class StringCalculatorKataTest {
+
+    private StringCalculatorKata calculatorKata;
+
+    @BeforeEach
+    void init(){
+        calculatorKata = new StringCalculatorKata();
+    }
+
+    @Test
+    void addEmptyNumberShouldReturn0(){
+        final String number = "";
+        final int sumOfNumbers = calculatorKata.add(number);
+        assertThat(sumOfNumbers).isEqualTo(0);
+    }
+
+    @Test
+    void addOneAndTwoNumberShouldReturn3(){
+        final String number = "1,2";
+        final int sumOfNumbers = calculatorKata.add(number);
+        assertThat(sumOfNumbers).isEqualTo(3);
+    }
+
+}
